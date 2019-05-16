ee372fd67a8dfcb9b042174fb5d172e1ac812bb5
Nurul fauzia aziza <fauziaazizahnurul@gmail.com>
2019-05-16 21:52:26 +0700

    perbaikan database

--- login.sql
+++ login.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2019 at 06:57 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Generation Time: May 16, 2019 at 04:41 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 7.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `daftar2` (
-  `Username` varchar(15) NOT NULL,
+  `Username` varchar(10) NOT NULL,
   `Password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,8 +36,8 @@ CREATE TABLE `daftar2` (
 --
 
 INSERT INTO `daftar2` (`Username`, `Password`) VALUES
-('mhs123', 'mhs123'),
-('dosen1', 'dosen1');
+('dosen', '4567'),
+('mahasiswa', '1234');
 
 -- --------------------------------------------------------
 
@@ -48,8 +46,8 @@ INSERT INTO `daftar2` (`Username`, `Password`) VALUES
 --
 
 CREATE TABLE `daftaradmin` (
-  `username` varchar(15) NOT NULL,
-  `password` varchar(15) NOT NULL
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -66,10 +64,10 @@ INSERT INTO `daftaradmin` (`username`, `password`) VALUES
 --
 
 CREATE TABLE `datarruangan` (
-  `idruangan` varchar(15) NOT NULL,
-  `namaruangan` varchar(15) NOT NULL,
-  `lokasi` varchar(25) NOT NULL,
-  `keterangan` varchar(25) NOT NULL
+  `idruangan` varchar(10) NOT NULL,
+  `namaruangan` varchar(10) NOT NULL,
+  `lokasi` varchar(20) NOT NULL,
+  `keterangan` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -77,8 +75,8 @@ CREATE TABLE `datarruangan` (
 --
 
 INSERT INTO `datarruangan` (`idruangan`, `namaruangan`, `lokasi`, `keterangan`) VALUES
-('12', 'Aulaa', 'Gedung E', 'Meminjam'),
-('12', 'Aulaa', 'Gedung E', 'Meminjam');
+('C102', 'AULA', 'Gedung C', 'Kapasitas 100 orang, full AC'),
+('D103', 'LAB PRODI', 'Gedung D', 'Tersedia 50 komputer, full AC');
 
 -- --------------------------------------------------------
 
@@ -87,14 +85,49 @@ INSERT INTO `datarruangan` (`idruangan`, `namaruangan`, `lokasi`, `keterangan`) 
 --
 
 CREATE TABLE `peminjaman` (
-  `idpeminjam` varchar(15) NOT NULL,
-  `namapeminjam` varchar(25) NOT NULL,
-  `tanggalpinjam` int(10) NOT NULL,
-  `tanggalkembali` int(10) NOT NULL,
-  `idruang` varchar(15) NOT NULL,
-  `namaruang` varchar(15) NOT NULL
+  `idpeminjam` varchar(10) NOT NULL,
+  `namapeminjam` varchar(20) NOT NULL,
+  `tanggalpinjam` date NOT NULL,
+  `tanggalkembali` date NOT NULL,
+  `idruangan` varchar(10) NOT NULL,
+  `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-COMMIT;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`idpeminjam`, `namapeminjam`, `tanggalpinjam`, `tanggalkembali`, `idruangan`, `status`) VALUES
+('14117046', 'Annisa', '2019-05-04', '2019-05-05', 'D103', 'diproses'),
+('14117071', 'Nurul', '2019-05-02', '2019-05-03', 'C102', 'diproses');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `daftar2`
+--
+ALTER TABLE `daftar2`
+  ADD PRIMARY KEY (`Username`);
+
+--
+-- Indexes for table `daftaradmin`
+--
+ALTER TABLE `daftaradmin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `datarruangan`
+--
+ALTER TABLE `datarruangan`
+  ADD PRIMARY KEY (`idruangan`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`idpeminjam`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
